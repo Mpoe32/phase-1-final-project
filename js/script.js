0b193edbcc55aca570cc331ce3fee3fe0cc8fa30
mpoe philip <philip.mpoe@student.moringaschool.com>
2025-03-26 14:41:04 +0300

    fetching and looping through reviews

--- js/script.js
+++ js/script.js
@@ -3,13 +3,27 @@ document.addEventListener("DOMContentLoaded", fetchReviews); //DOM fully load
 const form = document.getElementById("reviewForm");
 form.addEventListener("submit", addReview);// get the review form element and adding event listener for form submissiion
 
+
 function fetchReviews() {
-    fetch("http://localhost:3000/reviews")//fetch data from JSON serrver
-    .then(Response => Response.json())
-    .then(data => {
-        document.getElementById("reviews").innerHTML = "";
-    })
+    fetch("http://localhost:3000/reviews")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("reviews").innerHTML = "";
+            data.forEach(review => {
+                const div = document.createElement("div");
+                div.innerHTML = `
+                    <h3>${review.title} by ${review.author}</h3>
+                    <p>${review.review}</p>
+                    <p>Rating: ${review.rating}/5</p>
+                    <button onclick="deleteReview(${review.id})">Delete</button>
+                    <button onclick="updateReview(${review.id})">Edit</button>
+                `;
+                document.getElementById("reviews").appendChild(div);
+            });
+        });
 }
+        
+    
 
 
 
