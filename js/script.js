dc341a249368e93e84267824c70c0cce01c7dde6
mpoe philip <philip.mpoe@student.moringaschool.com>
2025-03-26 13:49:46 +0300

    get the review form element and adding event listener for form submissiion

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentLoaded", fetchReviews);
+document.addEventListener("DOMContentLoaded", fetchReviews); //DOM fully load 
 
 const form = document.getElementById("reviewForm");
-form.addEventListener("submit", addReview);
+form.addEventListener("submit", addReview);// get the review form element and adding event listener for form submissiion
+
 
 
