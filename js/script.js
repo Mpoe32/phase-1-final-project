8a4fc76556c319ce179aa81f7cbfd297fd04cc37
mpoe philip <philip.mpoe@student.moringaschool.com>
2025-03-26 14:28:28 +0300

    fetching reviews

--- js/script.js
+++ js/script.js
@@ -3,5 +3,13 @@ document.addEventListener("DOMContentLoaded", fetchReviews); //DOM fully load
 const form = document.getElementById("reviewForm");
 form.addEventListener("submit", addReview);// get the review form element and adding event listener for form submissiion
 
+function fetchReviews() {
+    fetch("http://localhost:3000/reviews")//fetch data from JSON serrver
+    .then(Response => Response.json())
+    .then(data => {
+        document.getElementById("reviews").innerHTML = "";
+    })
+}
+
 
 
